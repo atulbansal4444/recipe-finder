d81d10f9a913afc3745f64b290ae440519ca1935
Atul Bansal <atulbansal4444@gmail.com>
2023-02-27 00:45:50 +0530

    blocked bec of api need free api for recipes.

--- src/components/SearchRecipe.jsx
+++ src/components/SearchRecipe.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import {
   Form,
   FormGroup,
@@ -8,24 +8,41 @@ import {
 } from "react-bootstrap";
 
 const SearchRecipe = () => {
+  const [ingradients, setIngradients] = useState('');
+  const [dish, setDish] = useState('');
+
+  const searchEngine = (ingradients, dish) => {
+    const url = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingradients}`;
+    // console.log(url);
+    fetch(url, { method: 'GET'})
+      .then(response => response.json())
+      .then(json => console.log(json));
+  };
+
   return (
     <Fragment>
       <Form inline="true">
         <FormGroup>
           <FormLabel>Ingradients</FormLabel>
           {' '}
-          <FormControl type="text" placeholder="garlic, chicken"/>
+          <FormControl
+            type="text"
+            // value={ingradients}
+            placeholder="garlic, chicken"
+            onChange={(e) => {setIngradients(e.target.value)}} />
         </FormGroup>
-        {' '}
         <br/>
         <FormGroup>
           <FormLabel>Dish</FormLabel>
           {' '}
-          <FormControl type="text" placeholder="adobo" />
+          <FormControl
+            type="text"
+            // value={dish}
+            placeholder="adobo"
+            onChange={(e) => {setDish(e.target.value)}} />
         </FormGroup>
-        {' '}
         <br/>
-        <Button>Submit</Button>
+        <Button onClick={() => {searchEngine(ingradients, dish)}}>Submit</Button>
       </Form>
     </Fragment>
   );
